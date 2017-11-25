59d3b9ff57fea761d61556fbf5b0e6e26a43cc9b
Saqib Butt <saqib.butt@globalnorthstar.com>
2017-11-25 12:05:56 +0500

    Services at home page

--- home/index.php
+++ home/index.php
@@ -118,16 +118,16 @@ function body(){
   </section>
   <!-- end section about -->
   <!-- section works -->
-  <section id="works" class="section gray" style="display:none">
+  <section id="works" class="section gray" >
     <div class="container">
       <div class="row">
         <div class="col-md-8 col-md-offset-2">
           <div class="heading">
-            <h3><span>Recent works</span></h3>
+            <h3><span>Services</span></h3>
           </div>
           <div class="sub-heading">
             <p>
-              We have a history of doing what our name implies, creating a visual language around the beliefs of the brands we work with.
+              When rains, look for the rainbow. When it gets dark, look for the start.
             </p>
           </div>
         </div>
@@ -135,24 +135,11 @@ function body(){
       <div class="row">
         <div class="col-md-12">
           <ul class="grid effect" id="grid">
-            <li>
-              <a class="fancybox" data-fancybox-group="gallery" title="Portfolio name" href="assets/img/portfolio/1.jpg">
-								<img src="../assets/img/portfolio/1.jpg" alt="" />
-							</a>
-            </li>
-            <li><a href="assets/img/portfolio/2.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/2.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/3.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/3.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/4.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/4.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/5.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/5.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/6.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/6.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/7.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/7.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/8.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/8.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/9.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/9.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/10.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/10.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/11.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/11.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/12.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/12.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/13.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/13.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/14.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/14.jpg" alt="" /></a></li>
+            <li><a href="../assets/img/portfolio/111.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name" ><img src="../assets/img/portfolio/111.jpg" alt="" /></a></li>
+            <li><a href="../assets/img/portfolio/112.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/112.jpg" alt="" /></a></li>
+            <li><a href="../assets/img/portfolio/113.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/113.jpg" alt="" /></a></li>
+            <li><a href="../assets/img/portfolio/114.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/114.jpg" alt="" /></a></li>
+            <li><a href="../assets/img/portfolio/115.png" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="../assets/img/portfolio/115.png" alt="" /></a></li>
           </ul>
         </div>
       </div>
